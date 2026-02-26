2598488fc33ca11cc1d22e6feca9a62e1a49c6ca
Kiều Minh <lkieu5789@gmail.com>
2026-02-26 12:21:09 +0700

    fix kafka bootstrap + pipeline config

---
+++ scripts/bronze.py
@@ -0,0 +1,56 @@
+import os
+from pyspark.sql import SparkSession
+
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID", "minioadmin")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY", "minioadmin123")
+aws_region = os.getenv("AWS_REGION", "us-east-1")
+minio_endpoint = os.getenv("S3_ENDPOINT", "http://minio:9000")
+
+spark = SparkSession.builder \
+    .appName("CreateWeatherBronze") \
+    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+    .config("spark.sql.catalog.iceberg", "org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.iceberg.type", "rest") \
+    .config("spark.sql.catalog.iceberg.uri", "http://iceberg-rest:8181") \
+    .config("spark.sql.catalog.iceberg.warehouse", "s3://iceberg/warehouse") \
+    .config("spark.sql.catalog.iceberg.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
+    .config("spark.sql.catalog.iceberg.s3.endpoint", minio_endpoint) \
+    .config("spark.sql.catalog.iceberg.s3.path-style-access", "true") \
+    .config("spark.sql.catalog.iceberg.s3.access-key-id", aws_access_key) \
+    .config("spark.sql.catalog.iceberg.s3.secret-access-key", aws_secret_key) \
+    .config("spark.sql.catalog.iceberg.s3.region", aws_region) \
+    .config("spark.sql.defaultCatalog", "iceberg") \
+    .getOrCreate()
+
+spark.sql("CREATE NAMESPACE IF NOT EXISTS weather")
+
+spark.sql("""
+CREATE TABLE IF NOT EXISTS weather.weather_bronze (
+    time STRING,
+    province STRING,
+    city STRING,
+    temperature DOUBLE,
+    temp_min DOUBLE,
+    temp_max DOUBLE,
+    humidity DOUBLE,
+    feels_like DOUBLE,
+    visibility DOUBLE,
+    precipitation DOUBLE,
+    cloudcover DOUBLE,
+    wind_speed DOUBLE,
+    wind_gust DOUBLE,
+    wind_direction DOUBLE,
+    pressure DOUBLE,
+    is_day BOOLEAN,
+    weather_code INT,
+    weather_main STRING,
+    weather_description STRING,
+    weather_icon STRING,
+    kafka_time TIMESTAMP,
+    load_at TIMESTAMP
+)
+USING iceberg
+PARTITIONED BY (province)
+""")
+
+spark.stop()
